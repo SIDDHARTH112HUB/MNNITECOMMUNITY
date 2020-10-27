8921d5d27bdc32a4216b6b93bbe781e8a973bfac
Siddharth <siddharth.maurya112@gmail.com>
2020-10-27 22:46:24 +0530

    Login and SIgnUp 1

---
+++ app/src/main/java/com/arsenal/mnnite_community/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.arsenal.mnnite_community;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText emailText, passText;
+    Button loginbtn, sugnUpbtn;
+    TextView forgotPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth= FirebaseAuth.getInstance();
+        emailText=findViewById(R.id.email);
+        passText= findViewById(R.id.password);
+        loginbtn=findViewById(R.id.login);
+        forgotPass = findViewById(R.id.forgetPassword);
+        sugnUpbtn =findViewById(R.id.signUp);
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.sendPasswordResetEmail(emailText.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(LoginActivity.this, "Email Sent", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+        sugnUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent =new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=emailText.getText().toString();
+                String password= passText.getText().toString();
+                if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Enter every detail", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Log In Successful", Toast.LENGTH_SHORT).show();
+                                        Intent dashboardIntent =new Intent(LoginActivity.this,Dashboard.class);
+                                        startActivity(dashboardIntent);
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Please enter correct details ", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                            });
+                }
+            }
+        });
+
+
+
+    }
+}
--- app/src/main/java/com/arsenal/mnnite_community/MainActivity.java
+++ app/src/main/java/com/arsenal/mnnite_community/MainActivity.java
@@ -1,14 +1,75 @@
 package com.arsenal.mnnite_community;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    EditText nameText,emailText,passwordText;
+    Button signupBtn , loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+        nameText=findViewById(R.id.editName);
+        emailText=findViewById(R.id.editEmail);
+        passwordText=findViewById(R.id.editPass);
+        signupBtn=findViewById(R.id.signupBtn);
+        loginBtn=findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent =new Intent(com.arsenal.mnnite_community.MainActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =nameText.getText().toString();
+                String email=emailText.getText().toString();
+                String password= passwordText.getText().toString();
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(name)||TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(com.arsenal.mnnite_community.MainActivity.this, "Enter Every Details", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.length()<6){
+                    Toast.makeText(com.arsenal.mnnite_community.MainActivity.this, "password must be atleast 6 charecter long", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.createUserWithEmailAndPassword(email,password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(com.arsenal.mnnite_community.MainActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(com.arsenal.mnnite_community.MainActivity.this,Dashboard.class));
+                                    }
+                                    else{
+                                        Toast.makeText(com.arsenal.mnnite_community.MainActivity.this, "Couldn't create a Account", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
